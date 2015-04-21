b002651d6c44cea09a183e7f003dcdd35eb03e8e
Tyler Pottle <tyler.pottle@gmail.com>
2015-04-21 14:57:53 -0700

    specify eventserializer in index, create, & update

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -6,19 +6,19 @@ module API
       def index
         @kid = Kid.find(params[:kid_id])
         events = @kid.reminders
-        render json: events, status: 200
+        render json: events, each_serializer: EventSerializer, status: 200
       end
 
       def create
         event = Reminder.new(event_params)
         event.save!
-        render json: event, status: 201, location: api_v1_kid_events_url(event[:id])
+        render json: event, serializer: EventSerializer, status: 201, location: api_v1_kid_events_url(event[:id])
       end
 
       def update
         event = Reminder.find(params[:id])
         event.update!(event_params)
-        render json: event, status: 200
+        render json: event, serializer: EventSerializer, status: 200
       end
 
       def destroy
@@ -31,7 +31,7 @@ module API
 
       def event_params
         params.require(:event).permit(:name, :type, :datetime, :amount, :temperature,
-                                      :height, :weight, :description)
+                                      :height, :weight, :description, :kid_id)
       end
     end
   end
