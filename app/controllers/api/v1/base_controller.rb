658364df79ffa67dfab83a47e88336fe2100b58f
Tim Heuett <tim.heuett@gmail.com>
2015-04-21 14:36:47 -0700

    patch up sessions

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,10 +1,22 @@
 module API
   module V1
     class BaseController < ApplicationController
-      before_filter :authenticate_user!
+      skip_before_action :verify_authenticity_token
+      before_action :authenticate_user_from_token!
+
       prepend_before_filter :get_auth_token
 
       private
+
+      def authenticate_user_from_token!
+        user_token = params[:auth_token].presence
+        user = user_token && User.find_by_authentication_token(user_token.to_s)
+        if user
+          sign_in user, store: false
+        else
+          render_401
+        end
+      end
 
       def default_serializer_options
         { root: false }
