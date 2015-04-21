35a8041557d0bbf947a009b0cdd7a4ddf78505f1
Tim Heuett <tim.heuett@gmail.com>
2015-04-21 07:54:18 -0700

    add kid#destroy unhappy path test

--- test/integration/kids/delete_test.rb
+++ test/integration/kids/delete_test.rb
@@ -12,4 +12,14 @@ class DeletingTest < ActionDispatch::IntegrationTest
 
     assert_equal 204, response.status
   end
+
+  test 'error when trying to delete a non-existant kid' do
+    kid = Kid.new(id: "Prince Humperdinck")
+
+    delete_kid(kid)
+
+    assert_equal 404, response.status
+    json_error = JSON.parse(response.body, symbolize_names: true)
+    assert_equal 'Object does not exist', json_error[:message]
+  end
 end
