6f9a34e7dd4864b40622916633739f93bcd37f30
Tyler Pottle <tyler.pottle@gmail.com>
2015-04-21 16:14:39 -0700

    tests passing with auth_tokens

--- test/integration/events/deleting_event_test.rb
+++ test/integration/events/deleting_event_test.rb
@@ -4,7 +4,8 @@ class DeletingEventsTest < ActionDispatch::IntegrationTest
   test 'deleting an event' do
     kid = kids(:Jimmy)
     event = reminders(:two)
-    delete "/api/v1/kids/#{kid.id}/events/#{event.id}"
+    user = users(:liam)
+    delete "/api/v1/kids/#{kid.id}/events/#{event.id}?auth_token=#{user[:authentication_token]}"
     assert_equal 204, response.status
     assert_equal 5, Reminder.count
   end
@@ -12,7 +13,8 @@ class DeletingEventsTest < ActionDispatch::IntegrationTest
   test 'deleting an event that does not exist' do
     kid = kids(:Jimmy)
     event = reminders(:two)
-    delete "/api/v1/kids/#{kid.id}/events/10"
+    user = users(:liam)
+    delete "/api/v1/kids/#{kid.id}/events/10?auth_token=#{user[:authentication_token]}"
     assert_equal 404, response.status
     assert_equal 6, Reminder.count
   end
