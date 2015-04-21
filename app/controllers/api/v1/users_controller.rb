115d064dcea273ea04dfed47b950ecf1de53412a
Michael Becco <e4e6d4d5@gmail.com>
2015-04-21 13:30:14 -0700

    Fix User Status

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,7 @@ class API::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: user, each_serializer: API::V1::UserSerializer, status: 200, status: 201, location: user
+      render json: user, each_serializer: API::V1::UserSerializer, status: 201, location: user
     else
       render json: user.errors, status: 422
     end
