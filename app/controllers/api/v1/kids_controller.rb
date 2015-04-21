7f0ab7adbfcd24772e394fba481fc3c0c78e2fa5
Tim Heuett <tim.heuett@gmail.com>
2015-04-20 19:24:20 -0700

    add kid#create for api

--- app/controllers/api/v1/kids_controller.rb
+++ app/controllers/api/v1/kids_controller.rb
@@ -9,4 +9,17 @@ class Api::V1::KidsController < ApplicationController
     @kid = Kid.find(params[:id])
     render json: @kid, status: 200
   end
+
+  def create
+    @kid = Kid.new(kid_params)
+    if @kid.save
+      render json: @kid, status: 201
+    end
+  end
+
+  protected
+
+  def kid_params
+    params.require(:kid).permit(:name, :dob, :insurance_id, :nurse_phone)
+  end
 end
