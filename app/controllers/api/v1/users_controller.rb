6818f68b5f7c88b9172985267907f9bfe9820ff2
Michael Becco <e4e6d4d5@gmail.com>
2015-04-21 13:00:25 -0700

    Fix User Test Error Code:

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,6 +6,8 @@ class API::V1::UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       render json: user, status: 201, location: user
+    else
+      render json: user.errors, status: 422
     end
   end
 
@@ -13,8 +15,9 @@ class API::V1::UsersController < ApplicationController
     user = User.find(params[:id])
     if user.update(user_params)
       render json: user, status: 200
+    else
+      render json: user.errors, status: 422
     end
-  else
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
+
 
   before_save :ensure_authentication_token
   def ensure_authentication_token
