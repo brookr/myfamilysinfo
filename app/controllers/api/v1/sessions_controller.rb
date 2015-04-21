2cca56b761c3ddaa2d53b00e7c52932da07e7841
Michael Becco <e4e6d4d5@gmail.com>
2015-04-21 13:16:03 -0700

    First RuboCop

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,17 +1,16 @@
 class API::V1::SessionsController < Devise::SessionsController
-  before_filter :authenticate_user!, :except => [:create]
-  before_filter :ensure_params_exist, :except => [:destroy]
+  before_filter :authenticate_user!, except: [:create]
+  before_filter :ensure_params_exist, except: [:destroy]
   respond_to :json
 
 def create
   respond_to do |format|
     format.json {
       resource = resource_from_credentials
-      #build_resource
       return invalid_login_attempt unless resource
 
       if resource.valid_password?(params[:password])
-        render :json => {:auth_token => resource.authentication_token }, success: true, status: :created
+        render json: { auth_token: resource.authentication_token }, success: true, status: :created
       else
         invalid_login_attempt
       end
@@ -20,22 +19,23 @@ def create
 end
 
   protected
-    def ensure_params_exist
-      return unless params[:email].blank? || params[:password].blank?
-      render :json=>{:message=>"missing user_login parameter"}, :status=>422
-    end
 
-    def invalid_login_attempt
-      render :json=> {:message=>"Error with your login or password"}, :status=>401
-    end
+  def ensure_params_exist
+    return unless params[:email].blank? || params[:password].blank?
+    render json: { message: "missing user_login parameter" }, status: 422
+  end
 
-    def resource_from_credentials
-      data = {email: params[:email]}
-      if res = resource_class.find_for_database_authentication(data)
-        if res.valid_password?(params[:password])
-          res
-        end
+  def invalid_login_attempt
+    render json: { message: "Error with your login or password" }, status: 401
+  end
+
+  def resource_from_credentials
+    data = { email: params[:email] }
+    if res = resource_class.find_for_database_authentication(data)
+      if res.valid_password?(params[:password])
+        res
       end
     end
+  end
 end
 
