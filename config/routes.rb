2474c1212607b3239b82132d657a34a11fa7bad7
Tim Heuett <tim.heuett@gmail.com>
2015-04-20 17:21:54 -0700

    add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Myfamilysinfo::Application.routes.draw do
-
   root :to => "home#index"
- devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
-
+  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
 
   get ':users/:id/', :to => "user#show"
 
-  #resources :users, :path => "user"
   resource :user, { controller: :users, only: :show } do
     resources :kids, only: [:new, :create] do
       resources :reminders, only: [:new, :create] do
@@ -15,4 +12,14 @@ Myfamilysinfo::Application.routes.draw do
   end
   resources :kids, only: [:destroy]
   resources :reminders, only: [:destroy, :update]
+
+  namespace :api do
+    namespace :v1 do
+      resources :kids, except:[:edit, :new] do
+        resources :events, except: [:show, :edit, :new]
+      end
+      resource :users, only: [:create, :update]
+      resource :sessions, only: [:create]
+    end
+  end
 end
