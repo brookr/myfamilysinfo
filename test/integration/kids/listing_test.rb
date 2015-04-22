05a540904417c7c54548c99c5d718dfa90b5e498
Tim Heuett <tim.heuett@gmail.com>
2015-04-21 23:07:01 -0700

    add test for kid listing scope

--- test/integration/kids/listing_test.rb
+++ test/integration/kids/listing_test.rb
@@ -2,16 +2,31 @@ require 'test_helper'
 require 'api_helper'
 
 class ListingTest < ActionDispatch::IntegrationTest
-  test 'the index endpoint returns a list of kids' do
-    kid_names = Kid.all.map(&:name)
+  test 'the index endpoint returns a list of kids for the user' do
+    user = users(:mother)
+    kid_names = user.kids.map(&:name)
 
-    get_kids users(:mother).authentication_token
+    get_kids user.authentication_token
     assert_equal 200, response.status
 
     json_response = JSON.parse(response.body, symbolize_names: true)
     json_names = json_response.map { |k| k[:name] }
     kid_names.each do |name|
       assert_includes json_names, name
+    end
+  end
+
+  test 'the index endpoint does not show kids belonging to different user' do
+    user = users(:mother)
+    other_user = users(:miles)
+    kid_names = other_user.kids.map(&:name)
+
+    get_kids user.authentication_token
+
+    json_response = JSON.parse(response.body, symbolize_names: true)
+    json_names = json_response.map { |k| k[:name] }
+    kid_names.each do |name|
+      refute_includes json_names, name
     end
   end
 
