8fe9aa139ce5f40e1a85e28a89a8321e8a79f9c6
Tim Heuett <tim.heuett@gmail.com>
2015-04-22 19:07:57 -0700

    add password to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150423011848) do
+ActiveRecord::Schema.define(version: 20150423020724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,9 @@ ActiveRecord::Schema.define(version: 20150423011848) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "authentication_token"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.string   "password_digest"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", using: :btree
