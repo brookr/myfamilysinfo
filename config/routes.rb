5d7878990ee2a8c2e34ce42c68dd3cd92c6732ee
Tim Heuett <tim.heuett@gmail.com>
2015-04-23 12:48:15 -0700

    remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,22 +4,13 @@ Myfamilysinfo::Application.routes.draw do
   root :to => 'angular#index'
   get 'app', to: 'angular#app'
 
-  resource :user, { controller: :users, only: :show } do
-    resources :kids, only: [:new, :create] do
-      resources :reminders, only: [:new, :create] do
-      end
-    end
-  end
-  resources :kids, only: [:destroy]
-  resources :reminders, only: [:destroy, :update]
-
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       resources :kids, except:[:edit, :new] do
         resources :events, except: [:show, :edit, :new]
       end
       resources :users, only: [:create, :update]
-      # resources :sessions, only: [:create]
+      resources :sessions, only: [:create]
     end
   end
 end
